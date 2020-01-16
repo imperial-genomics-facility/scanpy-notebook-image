a6f7edd86a6eb1988c75c89b8eb16f4cf8d00b8d
Avik Datta <reach4avik@yahoo.com>
2020-01-16 15:45:03 +0000

    added tool versions

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-. /home/vmuser/miniconda3/etc/profile.d/conda.sh
-conda activate notebook-env
-Rscript /home/vmuser/r_lib.r
-conda deactivate
--- r_lib.r
+++
@@ -1,3 +0,0 @@
-install.packages(c('devtools', 'gam', 'RColorBrewer', 'BiocManager'),repos="https://cloud.r-project.org/")
-update.packages(ask=F)
-BiocManager::install(c("scran","MAST","monocle","ComplexHeatmap","slingshot"))
